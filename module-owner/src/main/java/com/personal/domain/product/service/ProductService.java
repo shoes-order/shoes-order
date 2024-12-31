0a6f7d038ae60daa80e964d7cfef3a196af661fb
여준서 <duwnstj12@naver.com>
2024-12-31 21:17:45 +0900

    [fix] : 상품 crud 수정 완료

--- module-owner/src/main/java/com/personal/domain/product/service/ProductService.java
+++ module-owner/src/main/java/com/personal/domain/product/service/ProductService.java
@@ -29,14 +29,21 @@ public class ProductService {
 
     public Page<ProductResponse.Infos> getProducts(ProductRequest.GetProducts getProducts, Long storeId) {
         Pageable pageable = PageRequest.of(getProducts.page() - 1, getProducts.size());
-        // TODO : Cannot invoke "String.hashCode()" because "<local3>" is null 에러 발생
-        return productRepository.getProducts(getProducts, pageable,storeId);
+        // TODO : Cannot invoke "String.hashCode()" because "<local3>" is null 에러 발생 : 해결
+        return productRepository.getProducts(getProducts, pageable, storeId);
     }
 
     public ProductResponse.Info getProduct(Long storeId, Long productId) {
-        // TODO : Cannot invoke "com.personal.entity.product.Product.isDeleted()" because "product" is null 에러 발생!
+        // TODO : Cannot invoke "com.personal.entity.product.Product.isDeleted()" because "product" is null 에러 발생! : 해결
         //        해당 에러는 매장과 상품 아이디기반의 검증이 덜되어서 난 에러! 예를 들어 storeId가 1인 매장의 상품을 storeId 2라는 값과 조회하면 나는 에러
+
+        // 가게 유무 확인
+        storeCommonService.getStores(storeId);
+
+        // 가게의 상품 찾기
         Product product = productCommonService.getStoreProduct(storeId, productId);
+
+        //상품 삭제 유무 확인
         if (product.isDeleted()) {
             throw new NotFoundProductException(ResponseCode.NOT_FOUND_PRODUCT);
         }
@@ -76,12 +83,13 @@ public class ProductService {
 
     @Transactional
     public void updateProduct(ProductRequest.UpdateProduct updateProduct, Long productId, Long storeId, AuthUser authUser) {
-        // TODO : storeId 와 productId 기반으로 검증 후 상품 수정이 가능하도록 할 것
+        // TODO : storeId 와 productId 기반으로 검증 후 상품 수정이 가능하도록 할 것 : 해결
         Store store = storeCommonService.getStores(storeId);
+
         if (!authUser.getUserId().equals(store.getUser().getId())) {
             throw new StoreOwnerMismatchException(ResponseCode.FORBIDDEN_PRODUCTS_UPDATE);
         }
-        Product product = productCommonService.getProducts(productId);
+        Product product = productCommonService.getStoreProduct(storeId, productId);
 
         product.updateProducts(
                 updateProduct.type(),
@@ -97,13 +105,13 @@ public class ProductService {
 
     @Transactional
     public void deleteProduct(Long storeId, Long productId, AuthUser authUser) {
-        // TODO : 내가 가지고 온 storeId값은 2인데 storeId 1 매장의 상품을 삭제처리하는 문제점 발생
+        // TODO : 내가 가지고 온 storeId값은 2인데 storeId 1 매장의 상품을 삭제처리하는 문제점 발생 : 해결
         //        storeId와 productId을 합쳐서 하는 검증이 덜 됨
         Store store = storeCommonService.getStores(storeId);
         if (!authUser.getUserId().equals(store.getUser().getId())) {
             throw new StoreOwnerMismatchException(ResponseCode.FORBIDDEN_PRODUCTS_DELETE);
         }
-        Product product = productCommonService.getProducts(productId);
+        Product product = productCommonService.getStoreProduct(storeId, productId);
         product.updateIsDeleted(true);
     }
 
