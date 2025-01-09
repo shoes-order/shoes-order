2427399fc6f886975db81eb9ae40ff33a84bb42b
여준서 <duwnstj12@naver.com>
2025-01-09 15:29:23 +0900

    [fix] : 다중 조회 조건 처리 수정 완료(name,category가 없어도 조회 가능)

--- module-owner/src/main/java/com/personal/domain/product/repository/ProductDslRepositoryImpl.java
+++ module-owner/src/main/java/com/personal/domain/product/repository/ProductDslRepositoryImpl.java
@@ -37,7 +37,7 @@ public class ProductDslRepositoryImpl implements ProductDslRepository {
                 .where(
                         product.store.id.eq(storeId),
                         product.isDeleted.eq(false),
-                        searchProduct(getProducts.type(), getProducts.value()),
+                        searchProduct(getProducts.name(), getProducts.category()),
                         applyIsSoldCondition(getProducts.isSold())
                 )
                 .orderBy(getProducts.sort().equals("ASC") ? product.updatedAt.asc() : product.updatedAt.desc())
@@ -51,22 +51,21 @@ public class ProductDslRepositoryImpl implements ProductDslRepository {
                 .where(
                         product.store.id.eq(storeId),
                         product.isDeleted.eq(false),
-                        searchProduct(getProducts.type(), getProducts.value()),
+                        searchProduct(getProducts.name(), getProducts.category()),
                         applyIsSoldCondition(getProducts.isSold())
                 )
                 .fetchOne();
         return new PageImpl<>(results, pageable, totalCount);
     }
 
-    private BooleanExpression searchProduct(String type, String value) {
-        switch (type) {
-            case ("name"):
-                return value != null ? product.name.contains(value) : null;
-            case ("category"):
-                return value != null ? product.category.contains(value) : null;
-            default:
-                return null;
+    private BooleanExpression searchProduct(String name, String category) {
+        if (name != null) {
+            return product.name.contains(name);
         }
+        if (category != null) {
+            return product.category.contains(category);
+        }
+        return null;
     }
 
 
