c7413fb76fc8ec0737e9dcf888f1fb653616ca72
ballqs <ballqs_@naver.com>
2025-01-13 18:35:18 +0900

    [update] 상품 개수 관련 컬럼 추가

--- module-entity/src/main/java/com/personal/entity/store/Store.java
+++ module-entity/src/main/java/com/personal/entity/store/Store.java
@@ -40,6 +40,9 @@ public class Store extends BaseEntity {
     @Column(nullable = false)
     private boolean isDeleted = false;
 
+    @Column(nullable = false)
+    private Long productSaleCnt = 0L;
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
@@ -69,5 +72,9 @@ public class Store extends BaseEntity {
 
     public void updateIsDeleted(boolean isDeleted) {
         this.isDeleted = isDeleted;
+    }
+
+    public void updateProductSaleCnt(Long productSaleCnt) {
+        this.productSaleCnt = productSaleCnt;
     }
 }
